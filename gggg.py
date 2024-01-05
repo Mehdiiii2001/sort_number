a96795d2cc2f743cb820297b06f703da281738fa
Mehdiiii2001 <151466339+Mehdiiii2001@users.noreply.github.com>
2024-01-05 11:15:18 -0800

    Update gggg.py

--- gggg.py
+++ gggg.py
@@ -1,38 +1,27 @@
+from collections import deque
+
 class Queue:
- # max_size: size of Q
- # Q: Array 
-    def __init__(self, max_size):
-        self.max_size = max_size
-        self.Q = [0] * max_size
-        self.num = 0
-        self.first = 0
- 
-    def enqueue(self, item):
-        if self.num >= self.max_size:
-            raise Exception("Queue overflow")
-            self.Q[(self.num + self.first) % self.max_size] = item
-            self.num += 1
- 
- 
+    def __init__(self):
+        self.elements = deque()
+
+    def enqueue(self, data):
+        self.elements.append(data)
+
     def dequeue(self):
-        if self.num == 0:
-            raise Exception("Queue empty")
-            item = self.Q[self.first]
-            self.first = (self.first + 1) % self.max_size
-            self.num -= 1
-        return item
- 
- 
-    def front(self):
-        if self.num == 0:
-            raise Exception("Queue empty")
-        return self.Q[self.first]
- 
-    def is_empty(self):
-        return self.num == 0
- 
+        return self.elements.popleft()
+
     def size(self):
-        return self.num
- 
-    def is_full(self):
-        return self.num >= self.max_size
+        return len(self.elements)
+
+    def last_in_line(self):
+        return self.elements[-1]
+
+
+queue = Queue()
+
+queue.enqueue("Person1")
+queue.enqueue("Person2")
+queue.enqueue("Person3")
+
+print("Number of people in the queue: ", queue.size())
+print("Last person in the queue: ", queue.last_in_line())
